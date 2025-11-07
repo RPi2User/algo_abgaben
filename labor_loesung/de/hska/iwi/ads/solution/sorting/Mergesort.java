b8f21d23ec537dd888945900d0b2d89ce31d9eb8
RPi2User <florian@hatzfeld.biz>
2025-11-07 12:33:25 +0100

    Got Mergesort Tests working

--- labor_loesung/de/hska/iwi/ads/solution/sorting/Mergesort.java
+++ labor_loesung/de/hska/iwi/ads/solution/sorting/Mergesort.java
@@ -4,21 +4,30 @@ import de.hska.iwi.ads.sorting.AbstractMergesort;
 
 public class Mergesort<E extends Comparable<E>> extends AbstractMergesort<E> {
 
+    public E[] getB() {
+        return this.b;
+    }
+
+    @Override
+    public void sort(E [] a) {
+        b = a.clone();
+        mergesort(a, 0, a.length - 1);
+    }
 
     @Override
     protected void mergesort(E[] a, int left, int right) {
         if (left < right){
-            int m = (left + right) /2;
+            int m = (left + right) / 2;
             mergesort(a, left, m);
-            mergesort(a, m+1, right);
-            melt(a, left,m,right);
+            mergesort(a, m + 1, right);
+            merge(a, left, m, right);
         }
     }
 
-    protected void melt(E[] a, int left, int m, int right){
+    protected void merge(E[] a, int left, int m, int right){
         int _left = left;
-        int _right = m;
-        for (int i = left; i <= right; i++ ){
+        int _right = m + 1;
+        for (int i = left; i < right; i++ ){
             if(_right > right || (left <= m && (a[_left].compareTo(a[_right]) <= 0))){
                 this.b[i] = a[i];
                 _left++;
@@ -26,10 +35,10 @@ public class Mergesort<E extends Comparable<E>> extends AbstractMergesort<E> {
             else{
                 b[i] = a[_right];
                 _right++;
-                for (int o = left; o <= right; o++){
-                    a[i] = b[i];
-                }
             }
+        }
+        for (int i = left; i < right; i++){
+            a[i] = b[i];
         }
     }
 }
